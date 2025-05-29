e0a543d9a7d671db2cda6197f387352764f42a8e
soloplayer345 <hongtlse172336@fpt.edu.vn>
2025-05-29 17:18:16 +0700

    create employee repo

---
+++ WebAPI/Infrastructure/Repos/EmployeeRepo.cs
@@ -0,0 +1,15 @@
+﻿using Application.IRepos;
+using Domain.Entities;
+
+namespace Infrastructure.Repos
+{
+    public class EmployeeRepo : GenericRepo<Employee>, IEmployeeRepo
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeRepo(AppDbContext dbContext) : base(dbContext)
+        {
+            _appDbContext = dbContext;
+        }
+    }
+}
