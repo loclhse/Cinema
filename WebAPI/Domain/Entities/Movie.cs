bfe1e4bb32a80458e3ca1c3692eaa6d17229f3a7
User <Loclhse172459@fpt.edu.vn>
2025-06-10 14:48:18 +0700

    movies-showtime

--- WebAPI/Domain/Entities/Movie.cs
+++ WebAPI/Domain/Entities/Movie.cs
@@ -8,19 +8,19 @@ namespace Domain.Entities;
 
 public partial class Movie : BaseEntity
 {
-    //public int MovieId { get; set; }
+    public string? Title { get; set; }
 
-    //public string? Title { get; set; }
+    public string? Description { get; set; }
 
-    //public string? Description { get; set; }
+    public int? Duration { get; set; }
 
-    //public int? Duration { get; set; }
+    public string? Rating { get; set; }
 
-    //public string? Rating { get; set; }
+    public string? Genre { get; set; }
 
-    //public string? Genre { get; set; }
+    public DateOnly? ReleaseDate { get; set; }
 
-    //public DateOnly? ReleaseDate { get; set; }
+    public string? ImageUrl { get; set; } 
 
-    //public virtual ICollection<Showtime> Showtimes { get; set; } = new List<Showtime>();
+    public virtual ICollection<Showtime> Showtimes { get; set; } = new List<Showtime>();
 }
