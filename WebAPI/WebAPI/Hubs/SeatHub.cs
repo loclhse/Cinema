27982eca3f88a7f4a2dd892ec953142c5f909475
TuanKiet25 <kietntse183810@fpt.edu.vn>
2025-06-18 07:15:26 +0700

    [Implement]HangFire, SignalR

---
+++ WebAPI/WebAPI/Hubs/SeatHub.cs
@@ -0,0 +1,19 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebAPI.Hubs
+{
+    public class SeatHub : Hub
+    { 
+        private readonly SeatScheduleService _seatScheduleService;
+        public SeatHub(SeatScheduleService seatScheduleService)
+        {
+            _seatScheduleService = seatScheduleService;
+        }
+        public async Task HoldSeats(List<Guid> seatIds)
+        {
+            var heldSeats = await _seatScheduleService.HoldSeatAsync(seatIds);
+            await Clients.All.SendAsync("ReceiveHeldSeats", heldSeats);
+        }
+    }
+}
