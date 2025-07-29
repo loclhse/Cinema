984fc4289c789c33b079a3af18d06583a46a0eba
TuanKiet25 <kietntse183810@fpt.edu.vn>
2025-07-29 10:24:23 +0700

    .

--- WebAPI/Domain/Entities/AppUser.cs
+++ WebAPI/Domain/Entities/AppUser.cs
@@ -8,15 +8,10 @@ public partial class AppUser : BaseEntity
 {
     //Khóa chính Shared-PK (Shared với ApplicationUser.Id)
     public string? Email { get; set; }
-
     public string? IdentityCard { get; set; }
-
     public DateOnly? Dob { get; set; }
-
     public string? FullName { get; set; }
-
     public string? Phone { get; set; }
-
     public string? Address { get; set; }
     public string? Avatar { get; set; }
     public int Score { get; set; } = 0;
@@ -29,12 +24,4 @@ public partial class AppUser : BaseEntity
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
     public virtual ICollection<Subscription> Subscriptions { get; set; } = new List<Subscription>();
     public virtual ICollection<Redeem> Redeems { get; set; } = new List<Redeem>();
-
-    //public virtual ICollection<Promotion> Promotions { get; set; } = new List<Promotion>();
-
-    //public virtual ICollection<ScoreHistory> ScoreHistories { get; set; } = new List<ScoreHistory>();
-
-    //public virtual ICollection<Theater> Theaters { get; set; } = new List<Theater>();
-
-
 }
